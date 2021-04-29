286e9039a0baacd46b3824c3d1a85930f59b8325
jargill21 <jarreau_gill@hotmail.com>
2021-04-29 12:35:59 +0200

    used handlebars

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const handlebars = require('express-handlebars');
+
+app.set('view engine', 'handlebars');
+
+app.engine('handlebars', handlebars({
+  layoutsDir: '${__dirname}/views/layouts'
+
+}));
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('GameSetMatch')
-})
+  res.render('main', {layout: 'index'});
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-app.get('/about', (req, res) => {
-  res.send('about')
-})
